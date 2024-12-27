9cc2d2196ffa2dd078b3925f377dde90ed4366b1
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-27 14:47:18 +0900

    feat: setting 파일 분리

---
+++ config/settings/local.py
@@ -0,0 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-z78_wruf*gk2cp!_)r8e^vy(017jdiz+h9tnvs#gh02)2oa-3c"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
