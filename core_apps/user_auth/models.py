152f30b435ca42ce5b61dc1c0cec8637ad8b7729
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2025-01-10 14:20:40 +0900

    feat: user 모델 생성 및 암호 찾기 퀴즈 생성

--- core_apps/user_auth/models.py
+++ core_apps/user_auth/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .emails import send_account_locked_email
+from .managers import UserManager
+
+class User(AbstractUser):
+    class SecurityQuestions(models.TextChoices):
+        MAIDEN_NAME = (
+            "maiden_name",
+            _("Waht is your mother's maiden name?"),
+        )
+        FAVORITE_COLOR = (
+            "favorite_color",
+            _("What is your favorite color?"),
+        )
+        BIRTH_CITY = (
+            "birth_city",
+            _("What is the city where you were born?")
+        )
+        CHILDHOOD_FRIEND = (
+            "childhood_friend",
+            _("What is the name of your childhood best friend?")
+        )
